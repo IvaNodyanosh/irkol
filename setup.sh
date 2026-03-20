5172b428b52b3956103aa8ebc584920a74874825
Eoan <ivanodyanosh@gmail.com>
2026-03-20 15:32:08 +0100

    added change mail ports settings

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git clone https://github.com/mailcow/mailcow-dockerized /opt/mailcow-dockerized
-
-cd /opt/mailcow-dockerized
-./generate_config.sh
-docker compose up -d
-
-cd /opt/my-site
-docker compose up -d
